fb7f8c1c511fb8241d44539c65b1e9c4e94c41d5
Quixar <145463262+Quixar@users.noreply.github.com>
2023-10-09 11:28:33 +0300

    Add files via upload

---
+++ HW14.09/HW14.09.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	windowsOption(); // настройка окна консоли
+	cursorOption(); // настройки курсора
+	dataGenerate(); // генерация данных
+	printData(); // вывод данных
+	installationWall(); // установка стен
+	defenitionEntry(); //установка входа
+	defenitionExit(); // установка выхода
+	setColor(); // установка цвета
+	definitionHero(); // местоположение ГГ
+	movementHero(); // передвижение героя
+	takingDamage(); //получение урона
+	takingCoin(); // собирание монет
+	takingHeal(); // подбирание аптечки
+	ckeckExit(); // проверка выхода
+	checkWin(); //проверка победы
+	checkLose(); // проверка поражения
+}
